a0afb94349a06ca676ea8b740e08774e9a64dbd0
gshs23078 <129289574+gshs23078@users.noreply.github.com>
2024-03-26 16:16:14 +0900

    Create B.cpp

---
+++ B.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+int main(){
+	int open[3]={0}, close[3]={0};
+	bool ans=true;
+	string str;
+	cin >> str;
+	int l=str.length();
+	stack<int> st;
+	for(int i=0; i<l; i++){
+		if(str[i]=='(')
+			st.push(1);
+		else if(str[i]=='[')
+			st.push(2);
+		else if(str[i]=='{')
+			st.push(3);
+		else if(str[i]==')'){
+			if(st.empty() || (st.size() && st.top()==1))
+				st.pop();
+			else{
+				ans=false;
+				break;
+			}
+		}
+		else if(str[i]==']'){
+			if(st.empty() || (st.size() && st.top()==2))
+				st.pop();
+			else{
+				ans=false;
+				break;
+			}
+		}
+		else if(str[i]=='}'){
+			if(st.empty() || (st.size() && st.top()==3))
+				st.pop();
+			else{
+				ans=false;
+				break;
+			}
+		}
+	}
+	if(!st.empty()) ans=false;
+	if(ans) cout << "O";
+	else cout << "X";
+}
